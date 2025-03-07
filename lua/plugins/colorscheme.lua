a8a3a73464a8c8382fdcd8adbfb32e45e980412e
rishabh-j-23 <rjain23112002@gmail.com>
2025-03-07 08:57:39 +0530

    nvim colorschem: nightfox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,18 +1,27 @@
 return {
-
   -- Configure LazyVim to load tokyonight
   {
     "folke/tokyonight.nvim",
     lazy = true,
     priority = 1000,
+    enabled = false,
     opts = { style = "night" },
   },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    enabled = false,
+    config = function()
+      vim.cmd("colorscheme kanagawa-wave")
+    end,
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    priority = 1000,
+    enabled = true,
+    config = function()
+      vim.cmd("colorscheme carbonfox")
+    end,
+  },
 }
-
--- return {
---   {
---     "rebelot/kanagawa.nvim",
---     lazy = true,
---     priority = 1000,
---   },
--- }
